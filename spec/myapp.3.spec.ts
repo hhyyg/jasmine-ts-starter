29c247f753926af2921404a0fd4155b34f7556ea
hhyyg <hiroka.yago@gmail.com>
2018-11-18 14:24:29 +0900

    add test failed

---
+++ spec/myapp.3.spec.ts
@@ -0,0 +1,11 @@
+import { Message } from '../src/myapp';
+
+
+describe('myapp3', () => {
+    it('hello', () => {
+        expect('e').toEqual('message');
+    });
+    it('hello2', () => {
+        expect('e2').toEqual('message');
+    });
+});
